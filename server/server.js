c4d62168ba85d4a9a44687c2a64941303b252fde
Muzammil Waheed <muzammilwaheed101@gmail.com>
2023-07-18 23:56:58 -0700

    add error handling for async

--- server/server.js
+++ server/server.js
@@ -18,33 +18,41 @@ const pool = new Pool({
 });
 
 const productInfo = async (req) => {
-  const product = pool.query(`SELECT * FROM product WHERE id=${req.params.product_id}`);
-  const features = pool.query(`SELECT feature, value FROM feature WHERE product_id=${req.params.product_id}`)
-  const promises = await Promise.all([product, features]);
-  return promises;
+  try {
+    const product = pool.query(`SELECT * FROM product WHERE id=${req.params.product_id}`);
+    const features = pool.query(`SELECT feature, value FROM feature WHERE product_id=${req.params.product_id}`)
+    const promises = await Promise.all([product, features]);
+    return promises;
+  } catch {
+    console.error('something went wrong in productInfo');
+  }
 }
 
 const productStyles = async (req) => {
-  const product = {id: req.params.product_id};
-  const styles = await pool.query(`SELECT * FROM style WHERE product_id=${req.params.product_id}`);
-  product.results = styles.rows;
-  product.results.forEach(style => {
-    style.photos = [];
-    style.skus = {};
-  });
+  try {
+    const product = {id: req.params.product_id};
+    const styles = await pool.query(`SELECT * FROM style WHERE product_id=${req.params.product_id}`);
+    product.results = styles.rows;
+    product.results.forEach(style => {
+      style.photos = [];
+      style.skus = {};
+    });
 
-  const stylePhotos = Promise.all(product.results.map(async style => {
-    const res = await pool.query(`SELECT thumbnail_url, url FROM photo WHERE style_id=${style.id}`);
-    style.photos = res.rows;
-  }));
+    const stylePhotos = Promise.all(product.results.map(async style => {
+      const res = await pool.query(`SELECT thumbnail_url, url FROM photo WHERE style_id=${style.id}`);
+      style.photos = res.rows;
+    }));
 
-  const styleSkus = Promise.all(product.results.map(async style => {
-    const res = await pool.query(`SELECT id, size, quantity FROM sku WHERE style_id=${style.id}`);
-    res.rows.forEach(sku => style.skus[sku.id] = {size: sku.size, quantity: sku.quantity});
-  }));
+    const styleSkus = Promise.all(product.results.map(async style => {
+      const res = await pool.query(`SELECT id, size, quantity FROM sku WHERE style_id=${style.id}`);
+      res.rows.forEach(sku => style.skus[sku.id] = {size: sku.size, quantity: sku.quantity});
+    }));
 
-  const promises = await Promise.all([stylePhotos, styleSkus])
-  return product;
+    const promises = await Promise.all([stylePhotos, styleSkus])
+    return product;
+  } catch {
+    console.error('something went wrong in productStyles');
+  }
 };
 
 app.use(cors());
@@ -67,11 +75,15 @@ app.get('/products/:product_id', async (req, res) => {
   if (req.params.product_id < 1) {
     res.send('product id is incorrect');
   } else {
-    const fullProduct = await productInfo(req);
-    const product = fullProduct[0].rows[0];
-    const features = fullProduct[1].rows;
-    product.features = features;
-    res.send(product);
+    try {
+      const fullProduct = await productInfo(req);
+      const product = fullProduct[0].rows[0];
+      const features = fullProduct[1].rows;
+      product.features = features;
+      res.send(product);
+    } catch {
+      console.error('something went wrong in product id endpoint');
+    }
   }
 });
 
@@ -79,8 +91,12 @@ app.get('/products/:product_id/styles', async (req, res) => {
   if (req.params.product_id < 1) {
     res.send('product id is incorrect');
   } else {
-    const fullProduct = await productStyles(req);
-    res.send(fullProduct);
+    try {
+      const fullProduct = await productStyles(req);
+      res.send(fullProduct);
+    } catch {
+      console.error('something went wrong in styles endpoint');
+    }
   }
 });
 
@@ -88,10 +104,14 @@ app.get('/products/:product_id/related', async (req, res) => {
   if (req.params.product_id < 1) {
     res.send('product id is incorrect');
   } else {
-    const {rows} = await pool.query(`SELECT related_id FROM related WHERE product_id=${req.params.product_id}`);
-    const related = [];
-    rows.map(r => related.push(r.related_id));
-    res.send(related);
+    try {
+      const {rows} = await pool.query(`SELECT related_id FROM related WHERE product_id=${req.params.product_id}`);
+      const related = [];
+      rows.map(r => related.push(r.related_id));
+      res.send(related);
+    } catch {
+      console.error('something went wrong in related endpoint');
+    }
   }
 });
 
